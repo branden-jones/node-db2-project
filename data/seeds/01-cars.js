d1bef14c2f3ff71d37cd7c8915a8d2e74f90b939
Branden Jones <branden.wayne.jones@gmail.com>
2022-11-16 18:26:34 -0500

    wip

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,29 @@
 // STRETCH
+const cars = [
+    {
+    vin: "1111111111111",
+    make: "toyota",
+    model: "prius",
+    mileage: 90000,
+    title: "clean",
+    transmission: 'manual'
+},
+    {
+    vin: "1111111111111",
+    make: "toyota",
+    model: "corola",
+    mileage: 10,
+    title: "salvage"
+},
+    {
+    vin: "1111111111111",
+    make: "ford",
+    model: "focus",
+    mileage: 300000,
+},
+]
+
+exports.seed = async function(knex) {
+    await knex('cars').truncate()
+    await knex('cars').insert(cars)
+}
