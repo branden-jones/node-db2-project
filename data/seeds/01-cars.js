9bb3f3a73bba5c8376b709ade13b5d4a6f9310da
Branden Jones <branden.wayne.jones@gmail.com>
2022-11-16 18:35:25 -0500

    wip

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,7 +1,7 @@
 // STRETCH
 const cars = [
     {
-    vin: "1111111111111",
+    vin: "11111111111111111",
     make: "toyota",
     model: "prius",
     mileage: 90000,
@@ -9,14 +9,14 @@ const cars = [
     transmission: 'manual'
 },
     {
-    vin: "1111111111111",
+    vin: "11111111111111111",
     make: "toyota",
     model: "corola",
     mileage: 10,
     title: "salvage"
 },
     {
-    vin: "1111111111111",
+    vin: "11111111111111111",
     make: "ford",
     model: "focus",
     mileage: 300000,
