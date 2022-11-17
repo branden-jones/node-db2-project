7d8aa26f435c69d34b2b3dde7d2916c16c02f565
Branden Jones <branden.wayne.jones@gmail.com>
2022-11-16 22:51:27 -0500

    server setup

--- api/server.js
+++ api/server.js
@@ -1,6 +1,20 @@
 const express = require("express")
 
 const server = express()
+server.use(express.json());
+
+server.use('*', (req,res,next) => {
+    next({ 
+        server: 404,
+        message: "Not Found... Marco..."
+    })
+})
+
+server.use((err,req,res,next) => { //eslint-disable-line
+    res.status(err.status || 500).json({
+        message: err.message
+    })
+})
 
 // DO YOUR MAGIC
 
