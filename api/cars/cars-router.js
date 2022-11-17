5ffa5e0d0fe171c4b0b9bfb17e184c382e3bb657
Branden Jones <branden.wayne.jones@gmail.com>
2022-11-16 23:57:43 -0500

    router connected

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,18 @@
-// DO YOUR MAGIC
+
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', async (req,res,next) => {
+    res.json('getting all cars')
+})
+
+router.get('/:id', async (req,res,next) => {
+    res.json('getting your special car')
+})
+
+router.post('/', async (req,res,next) => {
+    res.json('posting a new car')
+})
+
+module.exports = router
